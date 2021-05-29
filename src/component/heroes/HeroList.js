8b294c5de938cf9167bede2ebc141dda6b8f44d9
ignacioblanc <iiblanc@falabella.cl>
2021-05-29 15:16:33 -0400

    added new screen from data heroes, and added selectos

---
+++ src/component/heroes/HeroList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { getHeroesByPubliser } from '../../selectors/getHeroesByPublisher'
+
+export const HeroList = ({publisher}) => {
+
+    console.log(publisher)
+
+    const heroes = getHeroesByPubliser(publisher);
+    return (
+        <ul>
+            {
+            heroes.map(hero => (
+                <li key={hero.id}> 
+                    {hero.superhero}
+                </li>
+            ))
+            }
+            
+        </ul>
+        
+    )
+}
