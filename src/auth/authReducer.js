2c0fb24979c07647176f2a61092a3cc6b763b250
ignacioblanc <iiblanc@falabella.cl>
2021-06-03 13:46:45 -0400

    init context and reducer global for loggin

---
+++ src/auth/authReducer.js
@@ -0,0 +1,23 @@
+import { types } from "../types/types";
+// const state = {
+//     name: 'nacho',
+//     logged: true
+// }
+
+export const authReducer = (state = {}, action ) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                ...action.payload,
+                logged: true
+            }
+            break;
+        case types.logout:
+            return {
+                logged: false
+            }    
+    
+        default:
+            return state;
+    }
+}
